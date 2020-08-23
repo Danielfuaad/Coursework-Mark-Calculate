22afed09abd22096fe794e9000314294d3e66f72
Kyairulalifnajmi <69947097+Kyairulalifnajmi@users.noreply.github.com>
2020-08-23 22:39:46 +0800

    Update CourseworkMarkCalcualte.py
    add coding function lab assessment

--- CourseworkMarkCalcualte.py
+++ CourseworkMarkCalcualte.py
@@ -13,3 +13,31 @@ class App(Tk):
     self.addProj()
     self.addOutput()
     self.geometry("500x350")
+
+def addQuizzes(self):
+    Label(self, text = "Quizzes",
+          font = self.headerFont).grid(row=0,column =1)
+    Label(self,text = "10%",font = self.headerFont).grid(row=0,column=3)
+    
+    Label(self, text = "Quiz 1").grid(row = 1, column = 0)
+    self.txtQuiz1 = Entry(self)
+    self.txtQuiz1.grid(row = 1, column = 1)
+    self.txtQuiz1.insert(0, "")
+    Label(self,text = "4%").grid(row=1,column=2)
+
+    Label(self, text = "Quiz 2,3,4").grid(row = 2, column = 0)
+    self.txtQuiz2 = Entry(self)
+    self.txtQuiz2.grid(row = 2, column = 1)
+    self.txtQuiz2.insert(0, "")
+    Label(self,text = "5%").grid(row=2,column=2)
+
+def addLabAssessment(self):
+  Label(self, text = "Lab Assessment",
+          font = self.headerFont).grid(row=0,column =1)
+    Label(self,text = "10%",font = self.headerFont).grid(row=0,column=3)
+
+    Label(self, text = "Lab Asessment 1").grid(row = 1, column = 0)
+    self.txtLab1 = Entry(self)
+    self.txtLab1.grid(row = 1, column = 1)
+    self.txtLab1.insert(0, "")
+    Label(self,text = "4%").grid(row=1,column=2)
