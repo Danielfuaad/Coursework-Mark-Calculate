d0684f25b5f91c9932f26096cf502f19be62b18d
Danielfuaad <69518797+Danielfuaad@users.noreply.github.com>
2020-08-24 19:08:02 +0800

    Update CourseworkMarkCalcualte.py

--- CourseworkMarkCalcualte.py
+++ CourseworkMarkCalcualte.py
@@ -4,9 +4,10 @@ class App(Tk):
     def __init__(self):
         Tk.__init__(self)
 
-        self.headerFont = ("Helvetica", "16", "bold italic")
-        self.downFont = ("Helvetica", "60", "bold italic")
+        self.headerFont = ("Helvetica", "16", "bold")
+        self.downFont = ("Helvetica", "60", "bold")
         self.title("Coursework Mark Calculate")
+        
         self.addQuizzes()
         self.addLabAssessment()
         self.addLabTest()
@@ -14,101 +15,102 @@ class App(Tk):
         self.FinalAssessment()
         self.addOutput()
         
-        self.geometry("500x550")
+        self.geometry("500x560")
+    
     
     def addQuizzes(self):
-        Label(self, text = "Quizzes",font = self.headerFont).grid(row=0,column =1)
-        Label(self,text = "10%",font = self.headerFont).grid(row=0,column=3)
+        Label(self, text = "Quizzes",font = self.headerFont).grid(row=1,column =1)
+        Label(self,text = "10%",font = self.headerFont).grid(row=1,column=3)
     
-        Label(self, text = "Quiz 1").grid(row = 1, column = 0)
+        Label(self, text = "Quiz 1").grid(row = 2, column = 0)
         self.txtQuiz1 = Entry(self)
-        self.txtQuiz1.grid(row = 1, column = 1)
+        self.txtQuiz1.grid(row = 2, column = 1)
         self.txtQuiz1.insert(0, "")
-        Label(self,text = "4%").grid(row=1,column=2)
+        Label(self,text = "4%").grid(row=2,column=2)
 
-        Label(self, text = "Quiz 2,3,4").grid(row = 2, column = 0)
+        Label(self, text = "Quiz 2,3,4").grid(row = 3, column = 0)
         self.txtQuiz2 = Entry(self)
-        self.txtQuiz2.grid(row = 2, column = 1)
+        self.txtQuiz2.grid(row = 3, column = 1)
         self.txtQuiz2.insert(0, "")
-        Label(self,text = "5%").grid(row=2,column=2)
+        Label(self,text = "5%").grid(row=3,column=2)
 
     def addLabAssessment(self):
-        Label(self, text = "Lab Assessment",font = self.headerFont).grid(row=3,column =1)
-        Label(self,text = "25%",font = self.headerFont).grid(row=3,column=3)
+        Label(self, text = "Lab Assessment",font = self.headerFont).grid(row=4,column =1)
+        Label(self,text = "25%",font = self.headerFont).grid(row=4,column=3)
 
-        Label(self, text = "Lab Asessment 1").grid(row = 4, column = 0)
+        Label(self, text = "Lab Asessment 1").grid(row = 5, column = 0)
         self.txtLab1 = Entry(self)
-        self.txtLab1.grid(row = 4, column = 1)
+        self.txtLab1.grid(row = 5, column = 1)
         self.txtLab1.insert(0, "")
-        Label(self,text = "10%").grid(row=4,column=2)
+        Label(self,text = "10%").grid(row=5,column=2)
 
-        Label(self, text = "Lab tutorial ").grid(row = 5, column = 0)
+        Label(self, text = "Lab tutorial ").grid(row = 6, column = 0)
         self.txtLab2 = Entry(self)
-        self.txtLab2.grid(row = 5, column = 1)
+        self.txtLab2.grid(row = 6, column = 1)
         self.txtLab2.insert(0, "")
-        Label(self,text = "5%").grid(row=5,column=2)
+        Label(self,text = "5%").grid(row=6,column=2)
 
-        Label(self, text = "Lab tutorial 3,4,5,6 ").grid(row = 6, column = 0)
+        Label(self, text = "Lab tutorial 3,4,5,6 ").grid(row = 7, column = 0)
         self.txtLab3 = Entry(self)
-        self.txtLab3.grid(row = 6, column = 1)
+        self.txtLab3.grid(row = 7, column = 1)
         self.txtLab3.insert(0, "")
-        Label(self,text = "10%").grid(row=6,column=2)
+        Label(self,text = "10%").grid(row=7,column=2)
 
     def addLabTest(self):
-        Label(self, text = "Lab Test",font = self.headerFont).grid(row=7,column =1)
-        Label(self,text = "20%",font = self.headerFont).grid(row=7,column=3)
+        Label(self, text = "Lab Test",font = self.headerFont).grid(row=8,column =1)
+        Label(self,text = "20%",font = self.headerFont).grid(row=8,column=3)
     
-        Label(self, text = "Lab Assignment").grid(row = 8, column = 0)
+        Label(self, text = "Lab Assignment").grid(row = 9, column = 0)
         self.txtLabTest1 = Entry(self)
-        self.txtLabTest1.grid(row = 8, column = 1)
+        self.txtLabTest1.grid(row = 9, column = 1)
         self.txtLabTest1.insert(0, "")
-        Label(self,text = "10%").grid(row=8,column=2)
-
-        Label(self, text = "Code Review").grid(row = 9, column = 0)
-        self.txtLabTest2 = Entry(self)
-        self.txtLabTest2.grid(row = 9, column = 1)
-        self.txtLabTest2.insert(0, "")
         Label(self,text = "10%").grid(row=9,column=2)
 
+        Label(self, text = "Code Review").grid(row = 10, column = 0)
+        self.txtLabTest2 = Entry(self)
+        self.txtLabTest2.grid(row = 10, column = 1)
+        self.txtLabTest2.insert(0, "")
+        Label(self,text = "10%").grid(row=10,column=2)
+
     def addMiniProject(self):
-        Label(self, text = "Mini Project",font = self.headerFont).grid(row=10,column =1)
+        Label(self, text = "Mini Project",font = self.headerFont).grid(row=11,column =1)
         Label(self,text = "25%",font = self.headerFont).grid(row=10,column=3)
 
-        Label(self, text = "Report").grid(row = 11, column = 0)
+        Label(self, text = "Report").grid(row = 12, column = 0)
         self.txtReport = Entry(self)
-        self.txtReport.grid(row = 11, column = 1)
+        self.txtReport.grid(row = 12, column = 1)
         self.txtReport.insert(0, "")
-        Label(self,text = "10%").grid(row=11,column=2)
+        Label(self,text = "10%").grid(row=12,column=2)
     
-        Label(self, text = "Product & Presentation").grid(row = 12, column = 0)
+        Label(self, text = "Product & Presentation").grid(row = 13, column = 0)
         self.txtProduct = Entry(self)
-        self.txtProduct.grid(row = 12, column = 1)
+        self.txtProduct.grid(row = 13, column = 1)
         self.txtProduct.insert(0, "")
-        Label(self,text = "15%").grid(row=12,column=2)
+        Label(self,text = "15%").grid(row=13,column=2)
     
     def FinalAssessment(self):
-        Label(self, text = "Final Assessment",font = self.headerFont).grid(row = 13, column = 1)
+        Label(self, text = "Final Assessment",font = self.headerFont).grid(row = 14, column = 1)
         Label(self,text = "20%",font = self.headerFont).grid(row=13,column=3)
     
-        Label(self, text = "Final Assessment").grid(row = 14, column = 0)
+        Label(self, text = "Final Assessment").grid(row = 15, column = 0)
         self.txtFinal = Entry(self)
-        self.txtFinal.grid(row = 14, column = 1)
+        self.txtFinal.grid(row = 15, column = 1)
         self.txtFinal.insert(0, "")
-        Label(self,text = "20%").grid(row=14,column=2)
+        Label(self,text = "20%").grid(row=15,column=2)
 
     def addOutput(self):
-        self.btnCalc = Button(self, text = "calculate grade")
-        self.btnCalc.grid(row = 15, column = 1)
+        self.btnCalc = Button(self, text = "calculate grade and get data")
+        self.btnCalc.grid(row = 16, column = 1)
         self.btnCalc["command"] = self.calculate
-
-
-        Label(self, text = "Overall Percent").grid(row = 17, column = 0)
+        
+        Label(self, text = "Overall Percent").grid(row = 20, column = 0)
         self.lblTotal = Label(self, bg = "#fff", anchor = "w", relief = "groove")
-        self.lblTotal.grid(row = 17, column = 1, sticky = "we")
+        self.lblTotal.grid(row = 20, column = 1, sticky = "we")
 
-        Label(self, text = "Need To Pass").grid(row = 18, column = 0)
+        Label(self, text = "Need To Pass").grid(row = 21, column = 0)
         self.lblNeed = Label(self, bg = "#fff", anchor = "w", relief = "groove")
-        self.lblNeed.grid(row = 18, column = 1, sticky = "we")
+        self.lblNeed.grid(row = 21, column = 1, sticky = "we")
+
 
    
     def calculate(self):
@@ -143,15 +145,15 @@ class App(Tk):
         self.lblNeed["text"] = "%.2f" % NeedPass 
         
         if total >= 99.00:
-            Label(self, text = "A",font=self.downFont,bg="black",fg="green").grid(row = 19, column = 1)
+            Label(self, text = "A",font=self.downFont,bg="black",fg="green").grid(row = 22, column = 1)
         elif total >= 80.00: 
-            Label(self, text = "B",font=self.downFont,bg="black",fg="yellow").grid(row = 19, column = 1)
+            Label(self, text = "B",font=self.downFont,bg="black",fg="yellow").grid(row = 22, column = 1)
         elif total >= 70.00:
-            Label(self, text = "C",font=self.downFont,bg="black",fg="yellow").grid(row = 19, column = 1)
+            Label(self, text = "C",font=self.downFont,bg="black",fg="yellow").grid(row = 22, column = 1)
         elif total >= 60.00: 
-            Label(self, text = "D",font=self.downFont,bg="black",fg="yellow").grid(row = 19, column = 1)
+            Label(self, text = "D",font=self.downFont,bg="black",fg="yellow").grid(row = 22, column = 1)
         else : 
-            Label(self, text = "FAIL",font=self.downFont,bg="black",fg="red").grid(row = 19, column = 1)      
+            Label(self, text = "FAIL",font=self.downFont,bg="black",fg="red").grid(row = 22, column = 1)      
     
         
         
