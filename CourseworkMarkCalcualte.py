28f086c32612f704d1ea310df7884d25d853999f
Danielfuaad <69518797+Danielfuaad@users.noreply.github.com>
2020-08-24 19:16:58 +0800

    Update CourseworkMarkCalcualte.py

--- CourseworkMarkCalcualte.py
+++ CourseworkMarkCalcualte.py
@@ -74,7 +74,7 @@ class App(Tk):
 
     def addMiniProject(self):
         Label(self, text = "Mini Project",font = self.headerFont).grid(row=11,column =1)
-        Label(self,text = "25%",font = self.headerFont).grid(row=10,column=3)
+        Label(self,text = "25%",font = self.headerFont).grid(row=11,column=3)
 
         Label(self, text = "Report").grid(row = 12, column = 0)
         self.txtReport = Entry(self)
@@ -90,7 +90,7 @@ class App(Tk):
     
     def FinalAssessment(self):
         Label(self, text = "Final Assessment",font = self.headerFont).grid(row = 14, column = 1)
-        Label(self,text = "20%",font = self.headerFont).grid(row=13,column=3)
+        Label(self,text = "20%",font = self.headerFont).grid(row=14,column=3)
     
         Label(self, text = "Final Assessment").grid(row = 15, column = 0)
         self.txtFinal = Entry(self)
